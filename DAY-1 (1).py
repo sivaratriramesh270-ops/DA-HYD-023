cb5179f8dada341fe8ff79ed02f7d40bc583b6cc
sivaratriramesh270-ops <sivaratriramesh270@gmail.com>
2026-08-04 11:34:12 +0530

    tokens print day1.py

---
+++ DAY-1 (1).py
@@ -0,0 +1,56 @@
+#Tokens are the smallest units of a Python program.
+
+#Python has five types of tokens:
+
+#Keywords – Reserved words in Python (e.g., if, else, while, for, def).
+#Identifiers – Names given to variables, functions, and classes.
+#Literals – Constant values assigned to variables (e.g., 10, "Hello", 3.14).
+#Operators – Symbols that perform operations (e.g., +, -, *, /).
+#Punctuators – Symbols like (), {}, [], ,, :,. used for syntax structuring.
+
+#Sample Program
+
+print('hello world')
+
+''' This is a multi-line comment.
+It spans multiple lines.'''
+
+print("Hello, World!")
+
+print ('Hello codegnan' )
+'''
+Tokens - A smallest unit in a program
+'''
+name = ' codegnan'
+print (name)
+
+emailid = 'sasankvarma5050@gmail.com'
+print (emailid)
+
+email_id = 'sasankvarma5050@gmail.com'
+print (email_id)
+
+hunter='350' 
+print (hunter)
+
+batch = 'DA23!'
+
+print (batch)
+
+batch = 'cse'
+
+print (batch)
+
+print (batch)
+length = 5
+breadth =10
+
+area = length * breadth
+
+print (area)
+
+name, location, age= 'sasank', 'hyd', '21' 
+print (name) 
+print (location)
+print(age)
+
