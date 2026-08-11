780cee90f298aa0479051b5b410b9cddc4e2defe
sivaratriramesh270-ops <sivaratriramesh270@gmail.com>
2026-08-11 21:47:29 +0530

    trur or false print day13.py

---
+++ day13.py
@@ -0,0 +1,98 @@
+'''
+a = ("Welcome to python")
+print(a.lower())
+print(a.upper())
+print(a.title())
+print(a.capitalize())
+print(a.swapcase())
+'''
+a = input("Enter the Sentence")
+methods = ["upper","lower","title","capitalize","swapcase"]
+for i in methods:
+    if i =="upper":
+        print("upper",a.upper())
+    elif i =="lower":
+        print("lower:",a.lower())
+    elif i == "title":
+        print("lower:",a.lower())
+    elif i =="title":
+        print("title:",a.title())
+cii
+print("campitalize:",a.campitalize())
+    elif i =="swapcase":
+        print("swapcase:",a.swapcase())
+        
+      
+name=input("enter user name")
+method=[ "isalnum","isalpha","isidentifier","isascii"]
+
+while True
+    if name == "isalnum":
+        value = input("Enter a string:")
+        print("isalnum",name.isalnum())
+        continue
+    elif name == "isalpha":
+        value=input("enter a string:")
+        print("islapha",name.islapha())
+    elif name == "isidentifier":
+        
+name = input("Enter username: ")
+
+while True:
+
+    if name == "quit":
+        break
+
+    print("isalnum:", name.isalnum())
+
+    if name[0].isalpha():
+        print("First character is a letter")
+    else:
+        print("First character is not a letter")
+
+    print("isidentifier:", name.isidentifier())
+
+    print("isascii:", name.isascii())
+
+    name = input("\nEnter username or 'quit': ")
+
+print("Program ended")
+
+
+
+
+
+
+
+name = input("Enter username: ")
+
+while True:
+
+    if name == "quit":
+        break
+
+    print("isalnum:", name.isalnum())
+
+    if name[0].isalpha():
+        print("First character is a letter")
+    else:
+        print("First character is not a letter")
+
+    print("isidentifier:", name.isidentifier())
+
+    print("isascii:", name.isascii())
+
+    name = input("\nEnter username or 'quit': ")
+
+print("Program ended")
+
+
+
+
+
+
+
+
+
+
+        
